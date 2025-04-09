2501a4f0b2250ffa9971e701827974f74e78d2d4
Aaron Miller <aaronhmiller@users.noreply.github.com>
2025-04-09 16:54:09 -0700

    Update APIs.js fake SSN test

--- app/apis.js
+++ app/apis.js
@@ -8,6 +8,7 @@ const pool = new Pool({
 })
 
 const email=foo@example.io
+const ssn = 111-222-4444
 
 const getUsers = (req, res) => {
   pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
